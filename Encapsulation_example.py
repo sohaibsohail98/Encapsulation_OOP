4e68ab08149f5499588f1e09d6f956ebe2eeb7c1
S Sohail <ssohail@spartaglobal.com>
2020-07-06 00:01:57 +0100

    Encapsulation

---
+++ Encapsulation_example.py
@@ -0,0 +1,19 @@
+class Car:
+    __maximum_speed = 0
+    __name = ""
+
+    def __init__(self):
+        self.__maximum_speed = 200
+        self.__name = "Supercar"
+
+    def drive(self):
+        print('driving. maxspeed ' + str(self.__maximum_speed))
+
+    def setMaxSpeed(self, speed):
+        self.__maximum_speed = speed
+
+
+redcar = Car()
+redcar.drive()
+redcar.setMaxSpeed(320)
+redcar.drive()
